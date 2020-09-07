00b42aa4a9bd3c08d948efdf0fbe843faee6192e
NatumeMituan <NatumeMituan@gmail.com>
2020-09-07 15:52:01 +0800

    完善前端页面

--- static/js/overview.js
+++ static/js/overview.js
@@ -1,11 +1,35 @@
 const Overview_template = {
     template: "#overview_template",
+    data() {
+        return {
+            imgList: [],
+            vmList: [],
+        }
+    },
     methods: {
+        getImages: function () {
+            axios.get("/v1/images/").then((response) => {
+                console.log(response)
+                this.imgList = response.data.Images
+            }, (err) => {
+                console.log(err)
+            })
+        },
+        getVMs: function () {
+            axios.get("/v1/vms/").then((response) => {
+                console.log(response)
+                this.vmList = response.data.VMs
+            }, (err) => {
+                console.log(err)
+            })
+        },
         setMenuOption: function () {
             this.$parent.selectOption(0)
         }
     },
     mounted() {
         this.setMenuOption()
+        this.getImages()
+        this.getVMs()
     }
 }
